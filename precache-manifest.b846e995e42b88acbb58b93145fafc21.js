d00c38848a16579fdad06697b547d2088ae97ecb
Maria Terenteva <rtillkriyt@gmail.com>
2021-09-18 16:51:03 +0300

    Updates

---
+++ precache-manifest.b846e995e42b88acbb58b93145fafc21.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75dbc8a2ded4c1f92104",
+    "url": "/CRM-vue/css/app.4cd6f013.css"
+  },
+  {
+    "revision": "22b4100ab51ea1254075",
+    "url": "/CRM-vue/css/chunk-1af89739.385dd9a5.css"
+  },
+  {
+    "revision": "abc0823e575554715688",
+    "url": "/CRM-vue/css/chunk-vendors.24ce4a6c.css"
+  },
+  {
+    "revision": "b328a6ce315ded65f28bc18346101296",
+    "url": "/CRM-vue/index.html"
+  },
+  {
+    "revision": "75dbc8a2ded4c1f92104",
+    "url": "/CRM-vue/js/app.d3d03bdf.js"
+  },
+  {
+    "revision": "04ec1ffe76e7ef8f964d",
+    "url": "/CRM-vue/js/chunk-10b86dd9.914ebeda.js"
+  },
+  {
+    "revision": "22b4100ab51ea1254075",
+    "url": "/CRM-vue/js/chunk-1af89739.70c43018.js"
+  },
+  {
+    "revision": "e48f6f8ce4f66080ce15",
+    "url": "/CRM-vue/js/chunk-2d0d05dc.1acddd70.js"
+  },
+  {
+    "revision": "0b392bda73108fc538ac",
+    "url": "/CRM-vue/js/chunk-2d217a99.4885e6b2.js"
+  },
+  {
+    "revision": "835c674a2e6b9827c4a5",
+    "url": "/CRM-vue/js/chunk-2d21a3d2.267d416d.js"
+  },
+  {
+    "revision": "d887384a7c7796b30423",
+    "url": "/CRM-vue/js/chunk-53c8ec65.6dd7526f.js"
+  },
+  {
+    "revision": "ee1a714c11a1240f29a5",
+    "url": "/CRM-vue/js/chunk-5f4ad635.5a82574f.js"
+  },
+  {
+    "revision": "cb8f5b92803c4668e108",
+    "url": "/CRM-vue/js/chunk-bc3311b2.09cd63f3.js"
+  },
+  {
+    "revision": "0deeb1afbd48fd2281ca",
+    "url": "/CRM-vue/js/chunk-c420df12.02a551e2.js"
+  },
+  {
+    "revision": "abc0823e575554715688",
+    "url": "/CRM-vue/js/chunk-vendors.8c78c7b8.js"
+  },
+  {
+    "revision": "f2443b2c1cd86f728afe8d5bad9b96a0",
+    "url": "/CRM-vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/CRM-vue/robots.txt"
+  }
+]);
